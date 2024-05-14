a5f744f7f4b991b077c31d8702cfb4cc7da1a638
Khushanshu Yadav <khushanshuyadav2@gmail.com>
2024-05-14 13:14:05 +0530

    creating a seal class for screens

---
+++ app/src/main/java/com/khushanshu/recipeapp/Screen.kt
@@ -0,0 +1,9 @@
+package com.khushanshu.recipeapp
+
+//sealing the routes of screen fo type safety and re-usability
+sealed class Screen (route:String){
+
+    //defining RecipeScreen and DetailScreen as singleton object by "object" keyword so that can be re-used inplace of route
+    object RecipeScreen:Screen("recipescreen")
+    object DetailScreen:Screen("detailscreen")
+}
